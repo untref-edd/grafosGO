8431d59e38e1380111fd90866f53b5edf43c6bb8
Martin Albarracin <mafranzone@untref.edu.ar>
2023-08-12 19:45:04 -0300

    Se arregla Dijkstra

--- dijkstra.go
+++ dijkstra.go
@@ -15,24 +15,28 @@ func Dijkstra(g *Grafo, origen string) (map[string]int, map[string]string, error
 	// Inicializamos las estructuras auxiliares
 	distancia := make(map[string]int)
 	padre := make(map[string]string)
+	visitado := make(map[string]bool)
 	heap := Heap.NuevoHeapMin(func(i, j string) int {
 		return distancia[i] - distancia[j]
 	})
 	for _, v := range g.vertices {
 		distancia[v.nombre] = math.MaxInt32
 		padre[v.nombre] = ""
+		visitado[v.nombre] = false
 	}
 	distancia[origen] = 0
 	heap.Insertar(origen)
 	// Recorremos el heap
 	for !heap.EstaVacio() {
-		nombre, _ := heap.Extraer()
-		for _, arista := range g.vertices[nombre].aristas {
-			if distancia[arista.Destino()] > distancia[nombre]+arista.Peso() {
-				distancia[arista.Destino()] = distancia[nombre] + arista.Peso()
-				padre[arista.Destino()] = nombre
+		v, _ := heap.Extraer()
+		visitado[v] = true
+		// Recorremos los adyacentes
+		for _, arista := range g.vertices[v].aristas {
+			if distancia[arista.Destino()] > distancia[v]+arista.Peso() {
+				distancia[arista.Destino()] = distancia[v] + arista.Peso()
+				padre[arista.Destino()] = v
 				heap.Insertar(arista.Destino())
-			}
+			}	
 		}
 	}
 	return distancia, padre, nil
